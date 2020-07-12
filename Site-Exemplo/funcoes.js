98257865fa4cad26dcbb189cd9c6021a2b2046db
wenegawama <68117130+wenegawama@users.noreply.github.com>
2020-07-12 09:32:35 -0300

    Iniciando javascript e aprendendo git e github
    Tudo bem

---
+++ Site-Exemplo/funcoes.js
@@ -0,0 +1,12 @@
+<script language="javascript" src="_javascript/funcoes.js">
+<img id="icone" src="_imagens/glass-oculos-preto-peq.png"/>
+
+<nav id="menu">
+<h1>Menu Principal</h1> 
+<ul type="disc">
+    <li onmouseover="mudaFoto('_imagens/home.png')" onmouseout="mudaFoto('_imagens/glass-oculos-preto-peq.png')"><a href="_textos/index-txt.html">Home</a></li> 
+    <li onmouseover="mudaFoto('_imagens/especificacoes.png')" onmouseout="mudaFoto('_imagens/glass-oculos-preto-peq.png')"><a href="_textos/specs-txt.html">Especificações</a></li> 
+    <li onmouseover="mudaFoto('_imagens/fotos.png')" onmouseout="mudaFoto('_imagens/glass-oculos-preto-peq.png')"><a href="_textos/fotos-txt.html">Fotos</a></li>
+    <li onmouseover="mudaFoto('_imagens/multimidia.png')" onmouseout="mudaFoto('_imagens/glass-oculos-preto-peq.png')"><a href="_textos/multimidia-txt.html">Multimídia</a> </li>
+    <li onmouseover="mudaFoto('_imagens/contato.png')" onmouseout="mudaFoto('_imagens/glass-oculos-preto-peq.png')"><a href="_textos/fale-conosco-txt.html">Fale conosco</a> </li>
+</script>
